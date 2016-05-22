91c4a50ed216c1ec17e52d783dbc660753310619
Vladislav <Streamline28@mail.ru>
2016-05-22 17:52:13 +0300

    Database changed

--- app/configs/DataConfig.java
+++ app/configs/DataConfig.java
@@ -1,7 +1,6 @@
 package configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -15,12 +14,6 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import play.Play;
-import scala.reflect.io.VirtualFile;
-
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.logging.Logger;
 
 @Configuration
 @EnableTransactionManagement
